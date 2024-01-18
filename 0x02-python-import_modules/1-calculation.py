8c919e02c6b1e37d755f0818f89ae82a54a95955
cynthiakoskei <koskei.thia@gmail.com>
2024-01-18 11:33:54 -0800

    my first toolbox

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ = "__main__":
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, (a+b)))
+    print("{} + {} = {}".format(a, b, (a-b)))
+    print("{} + {} = {}".format(a, b, (a*b)))
+    print("{} + {} = {}".format(a, b, int(a/b)))
